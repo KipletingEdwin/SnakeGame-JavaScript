de37a78a1176d7ce13dd2d2092c2a5a8b3325235
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-05-06 11:54:57 +0100

    add some eventListeners

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ let yVelocity = 0;
 let foodX;
 let foodY;
 let score = 0;
+let snake = [
+    {x:unitSize * 4, y:0},
+    {x:unitSize * 3, y:0},
+    {x:unitSize * 2, y:0},
+    {x:unitSize * 1, y:0},
+    {x:unitSize, y:0},
+    {x:0,y:0}
+]
+
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click",resetGame);
+
+gameStart();
